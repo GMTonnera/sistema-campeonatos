cc6d9844195907bdf7e42905889ebd6e1bcbff0e
gmtonnera <gmtonnera@gmail.com>
2026-02-26 16:16:14 -0300

    feat: match status enum

---
+++ apps/api/src/common/enums/match-status.enum.ts
@@ -0,0 +1,7 @@
+export enum MatchStatus {
+    UNSCHEDULED = 'unscheduled',
+    SCHEDULED = 'scheduled',
+    IN_PROGRESS = 'in progress',
+    FINISHED = 'finished',
+    CANCELLED = 'cancelled',
+}
